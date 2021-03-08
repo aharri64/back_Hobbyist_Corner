c8b34e439d0c2c5983f7ffa7ce02d791ac9395af
scott95100 <scott95100@gmail.com>
2021-03-08 13:29:56 -0700

    add projectUpdate.js - add project update schema

---
+++ models/projectUpdate.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const projectUpdateSchema = new Schema({
+    projectUpdateName: {
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    descriptionUpdate: {
+        type: String,
+    },
+    imageUpdate: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    comment: {
+        type: String,
+        ref: 'comment_id'
+    }
+});
+
+const projectUpdate = mongoose.model('ProjectUpdate', projectUpdateSchema);
+
+module.exports = projectUpdate;
