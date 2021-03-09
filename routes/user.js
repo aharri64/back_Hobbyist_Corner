2d18e12fc48df6fbfa20ea485e53cc8a50ca6a7e
aharri64 <haveacompleteday@gmail.com>
2021-03-09 14:58:46 -0700

    add routes for profile

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,8 @@ const passport = require('passport');
 router.get('/test', ctrl.user.test);
 router.post('/register', ctrl.user.register);
 router.post('/login', ctrl.user.login);
-// router.get('/profile', passport.authenticate('jwt', { session: false }), ctrl.user.profile); // session: false ???
+router.get('/profile', passport.authenticate('jwt', { session: false }), ctrl.user.profile); // session: false ???
+router.post('/profile', passport.authenticate('jwt', { session: false }), ctrl.user.profilePost); // session: false ???
 router.get('/messages', passport.authenticate('jwt', { session: false }), ctrl.user.messages); // session: false ???
 
 
